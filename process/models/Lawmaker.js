b2e694e1ddeef9e2653fbfd2f96133dd8b128f21
eidietrich <eric.dietrich@gmail.com>
2022-12-28 11:42:16 -0700

    Clean up Lawmaker file

--- process/models/Lawmaker.js
+++ process/models/Lawmaker.js
@@ -69,7 +69,7 @@ export default class Lawmaker {
 
             imageSlug: image_path.replace('images/', ''),
 
-            // Merge this stuff in later
+            // Merge this stuff in later, see main.js
             // votingSummary: this.getVotingSummary(lawmaker, this.votes), 
             // voteTabulation: this.getVoteTabulation(lawmaker, filterToFloorVotes(this.votes)), // BIG DATA
             // keyBillVotes: [], // TODO,
@@ -84,13 +84,6 @@ export default class Lawmaker {
         const replacement = LAWMAKER_REPLACEMENTS.find(d => d.district === districtKey)
         return replacement && replacement.note || null
     }
-    // getLocales = (lawmaker, districts) => {
-    //     const district = districts.find(d => d.key === lawmaker.district)
-    //     return {
-    //         short: district.locale,
-    //         long: district.locale_description
-    //     }
-    // }
 
     addSponsoredBills = ({ sponsoredBills }) => {
         this.sponsoredBills = sponsoredBills.map(bill => {
@@ -141,107 +134,6 @@ export default class Lawmaker {
         })
         return lawmakerVotes
     }
-    getVoteTabulation = (lawmaker, lawmakerVotes) => {
-        // TODO: Fine-tune this to floor votes (currently done w/ inputs), one per bill
-        // Display will need an 'is this a key vote' filter
-        return lawmakerVotes.map(vote => {
-            return {
-                billKey: billKey(vote.data.bill),
-                lawmakerVote: vote.votes.find(d => d.name === lawmaker.name).option,
-                bill: vote.data.bill,
-                action: vote.data.action,
-                date: vote.data.date,
-                keyVote: true,
-                count: vote.data.count,
-                motionPassed: vote.data.motionPassed,
-                gopCount: vote.data.gopCount,
-                gopSupported: vote.data.gopSupported,
-                demCount: vote.data.demCount,
-                demSupported: vote.data.demSupported,
-            }
-        })
-    }
-    getVoteAlignments = (lawmaker, floorVotes) => {
-        // % of times voting w/ peer lawmakers on floor
-        const lawmakerVotes = floorVotes.map(vote => vote.data.votes.find(d => d.name === lawmaker.name))
-        const peers = Array.from(new Set(floorVotes.map(vote => vote.data.votes.map(d => d.name)).flat()))
-        const alignments = peers.map(peerName => {
-            const alignmentArray = floorVotes.map(vote => {
-                const lawmakerVote = vote.data.votes.find(d => d.name === lawmaker.name)
-                const peerVote = vote.data.votes.find(d => d.name === peerName)
-                return (lawmakerVote.option === peerVote.option) ? 1 : 0
-            })
-            const alignmentScore = alignmentArray.reduce((i, acc) => i + acc, 0) / floorVotes.length
-
-            const peerFirstVote = floorVotes[0].data.votes.find(d => d.name === peerName)
-            const peerDistrict = peerFirstVote.district
-            const peerParty = peerFirstVote.party
-
-            return {
-                name: peerName,
-                district: peerDistrict,
-                party: peerParty,
-                alignment: alignmentScore,
-            }
-        })
-        return alignments
-
-    }
-
-    // old
-    getVotingSummary = (lawmaker, lawmakerVotes) => {
-
-        // const floorVotes = filterToFloorVotes(lawmakerVotes)
-        const floorVotes = lawmakerVotes
-        const voteTabulation = this.getVoteTabulation(lawmaker, floorVotes)
-        // const voteAlignments = this.getVoteAlignments(lawmaker, floorVotes) // costly function
-
-        const numVotesRecorded = floorVotes.length
-        const numVotesNotPresent = voteTabulation.filter(d => !['yes', 'no'].includes(d.lawmakerVote)).length
-        const numVotesCast = numVotesRecorded - numVotesNotPresent
-        const votesWithMajority = voteTabulation.filter(d =>
-            ((d.lawmakerVote === 'yes') && d.motionPassed)
-            || ((d.lawmakerVote === 'no') && !d.motionPassed)
-        ).length
-        const votesWithGopMajority = voteTabulation.filter(d =>
-            ((d.lawmakerVote === 'yes') && d.gopSupported)
-            || ((d.lawmakerVote === 'no') && !d.gopSupported)
-        ).length
-        const votesWithDemMajority = voteTabulation.filter(d =>
-            ((d.lawmakerVote === 'yes') && d.demSupported)
-            || ((d.lawmakerVote === 'no') && !d.demSupported)
-        ).length
-
-        const votingSummary = {
-            numVotesRecorded,
-            numVotesCast,
-            numVotesNotPresent,
-            fractionVotesNotPresent: (numVotesNotPresent / numVotesRecorded) || 0,
-            votesWithMajority,
-            fractionVotesWithMajority: (votesWithMajority / numVotesCast) || 0,
-            votesWithGopMajority,
-            fractionVotesWithGopMajority: (votesWithGopMajority / numVotesCast) || 0,
-            votesWithDemMajority,
-            fractionVotesWithDemMajority: (votesWithDemMajority / numVotesCast) || 0,
-            // voteAlignments,
-        }
-        return votingSummary
-
-    }
-
-    getArticles = (lawmaker, articles) => {
-        const articlesAboutLawmaker = articles.filter(article => article.data.lawmakerTags.includes(lawmaker.name)).map(d => d.export())
-        // if (articlesAboutLawmaker.length > 0) console.log(lawmaker.name, articlesAboutLawmaker.length)
-        return articlesAboutLawmaker
-    }
-
-    getAnnotation = (lawmaker, annotations) => {
-        const match = annotations.lawmakers.find(d => d.key === lawmaker.name)
-        // if (match) console.log('Lawmaker annotation found for', lawmaker.name)
-        const annotation = (match && match.annotation) || []
-        return annotation
-    }
-
 
     exportMerged = () => {
         return {
