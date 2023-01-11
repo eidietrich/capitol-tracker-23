ccf56842e6601085c99f2bd1f8989377de47af1d
eidietrich <eric.dietrich@gmail.com>
2023-01-11 08:02:46 -0700

    Calendar page troubleshooting

--- process/models/CalendarPage.js
+++ process/models/CalendarPage.js
@@ -3,6 +3,12 @@ import { dateParse } from '../functions.js'
 export default class CalendarPage {
     constructor({ actions, bills, updateTime, calendarAnnotations }) {
         const beginningOfToday = new Date(updateTime).setUTCHours(7, 0, 0, 0) // 7 accounts for Montana vs GMT time
+        console.log({
+            updateTime,
+            beginningOfToday: new Date(beginningOfToday),
+            parseCheck: dateParse('01/11/2023'),
+            compare: new Date(beginningOfToday) >= beginningOfToday,
+        })
         const todayOrLaterActions = actions.filter(d => dateParse(d.date) >= beginningOfToday)
         const scheduledHearings = todayOrLaterActions.filter(d => d.hearing)
         const scheduledFloorDebates = todayOrLaterActions.filter(d => d.scheduledForFloorDebate)
