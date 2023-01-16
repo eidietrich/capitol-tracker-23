52db0f9e1209ec8360e98c61fdb4b0f1b0a4f844
eidietrich <eric.dietrich@gmail.com>
2023-01-16 14:52:33 -0700

    Add replacement notes

--- process/config/overrides.js
+++ process/config/overrides.js
@@ -9,7 +9,9 @@ export const MANUAL_VETOS = [
 
 export const LAWMAKER_REPLACEMENTS = [
     // TODO
-    { district: 'SD 40', note: 'Becky Beard was appointed to represent SD 40 after the Nov. 2022 resignation of Sen. Terry Gauthier.' }
+    { district: 'SD 40', note: 'Becky Beard was appointed to represent SD 40 after the Nov. 2022 resignation of Sen. Terry Gauthier.' },
+    { district: 'HD 80', note: 'Zack Wirth was appointed to represent HD 80 after Rep. Becky Beard was appointed to fill a vacant Senate seat in Dec. 2022.' },
+    { district: 'HD 29', note: 'Ed Butcher was appointed to represent HD 28 after Rep. Doug Flament resigned in Jan. 2023.' },
     // Add post-elect replacements. 
     // Beard replacement
 ]
